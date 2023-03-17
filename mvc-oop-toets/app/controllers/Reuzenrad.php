2e81f177b1ea52e51a6b75f2d85cc9131a880c9a
TheManAdham <117640339+TheManAdham@users.noreply.github.com>
2023-03-17 13:08:19 +0100

    Update Reuzenrad.php

--- mvc-oop-toets/app/controllers/Reuzenrad.php
+++ mvc-oop-toets/app/controllers/Reuzenrad.php
@@ -2,34 +2,34 @@
 
 use LDAP\Result;
 
-    class Reuzenrad extends BaseController
-    {
-        private $reuzenradModel;
+class Reuzenrad extends BaseController
+{
+    private $reuzenradModel;
 
-        public function __construct()
-        {
-            $this->reuzenradModel = $this->model('ReuzenradModel');
-        }
+    public function __construct()
+{
+   $this->reuzenradModel = $this->model('ReuzenradModel');
+}
 
-        public function index()
-        {
-            $result = $this->reuzenradModel->getReuzenrad();
+public function index()
+{
+   $result = $this->reuzenradModel->getReuzenrad();
 //var_dump($result);
-            $rows = "";
-            foreach ($result as $reuzenrad) {
-                $rows .= "<tr>
-                            <td>$reuzenrad->Naam</td>
-                            <td>$reuzenrad->Hoogte</td>
-                            <td>$reuzenrad->Land</td>
-                            <td>$reuzenrad->Kosten</td>
-                            <td>$reuzenrad->AantalPassagiers</td>
-                          </tr>";
-            }
+    $rows = "";
+    foreach ($result as $reuzenrad) {
+    $rows .= "<tr>
+    <td>$reuzenrad->Naam</td>
+    <td>$reuzenrad->Hoogte</td>
+    <td>$reuzenrad->Land</td>
+    <td>$reuzenrad->Kosten</td>
+    <td>$reuzenrad->AantalPassagiers</td>
+    </tr>";
+     }
 
-            $data = [
-                'title' => 'Top 5 hoogste reuzenraden ter wereld',
-                'rows'  => $rows
-            ];
+      $data = [
+        'title' => 'Top 5 hoogste reuzenraden ter wereld',
+         'rows'  => $rows
+        ];
 
             $this->view('reuzenrad/index', $data);
         }
